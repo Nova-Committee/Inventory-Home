0d8918c2eefd970e3fd1550ed7ca0cd2b60d6d8d
cnlimiter <cnlimiter@163.com>
2022-03-28 13:45:19 +0800

    add recipes

--- src/main/java/nova/committee/inventoryhome/InventoryHome.java
+++ src/main/java/nova/committee/inventoryhome/InventoryHome.java
@@ -13,7 +13,6 @@ import nova.committee.inventoryhome.init.handler.SecurityHandler;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-// The value here should match an entry in the META-INF/mods.toml file
 @Mod("inventoryhome")
 public class InventoryHome {
 
